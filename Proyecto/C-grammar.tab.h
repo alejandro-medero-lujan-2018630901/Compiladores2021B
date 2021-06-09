da6da53f4604fe390cc8e47fa9418764f6c54089
alejandro-medero-lujan-2018630901 <85586804+alejandro-medero-lujan-2018630901@users.noreply.github.com>
2021-06-09 15:00:28 -0500

    Flex y bison

---
+++ Proyecto/C-grammar.tab.h
@@ -0,0 +1,81 @@
+/* A Bison parser, made by GNU Bison 3.0.4.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_C_GRAMMAR_TAB_H_INCLUDED
+# define YY_YY_C_GRAMMAR_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    IDENTIFIER = 258,
+    CONSTANT = 259,
+    INC_OP = 260,
+    DEC_OP = 261,
+    LE_OP = 262,
+    GE_OP = 263,
+    EQ_OP = 264,
+    NE_OP = 265,
+    MUL_ASSIGN = 266,
+    DIV_ASSIGN = 267,
+    MOD_ASSIGN = 268,
+    ADD_ASSIGN = 269,
+    CHAR = 270,
+    INT = 271,
+    VOID = 272,
+    IF = 273,
+    WHILE = 274,
+    RETURN = 275
+  };
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_C_GRAMMAR_TAB_H_INCLUDED  */
---
+++ Proyecto/archivo.c
@@ -0,0 +1,14 @@
+int main(){
+	int a = 10;
+
+	if(a < 10){
+		int b = 12;
+	}
+ 	a = 15;
+
+	while(a >= 10){
+		int c = 14;
+	}
+
+	return 0;
+}
